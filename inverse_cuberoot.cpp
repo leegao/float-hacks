ecd87e6a829b4e69466aa794151891f9151180b2
leegao <lg342@cornell.edu>
2016-12-07 13:39:30 -0800

    Tidy things up a bit

--- inverse_cuberoot.cpp
+++ inverse_cuberoot.cpp
@@ -5,12 +5,11 @@
 using namespace floathacks;
 
 int main() {
-    std::cout << std::get<0>(break_float(3.14)) << "/" << std::get<1>(break_float(3.14)) << std::endl;
-    std::cout << std::get<1>(break_float(0.5)) << std::endl;
-    std::cout << pow<FLOAT(0.5)>(2) << std::endl;
-    std::cout << consts::pow(2, 0.5) << std::endl;
-    std::cout << fpow<Ratio<1, 2>>::estimate(2) << std::endl;
-//    std::cout << consts::fpow(20, -0.5) << std::endl;
-//    std::cout << l2f(f2l(-3.256)) << std::endl;
+    // More precise (2 newton iterations)
+    std::cout << pow<FLOAT(-0.5f)>(3.1415926f) << std::endl;
+    std::cout << consts::pow(3.1415926f, -0.5f) << std::endl;
+    // Raw guesses
+    std::cout << fpow<FLOAT(-0.5f)>::estimate(3.1415926f) << std::endl;
+    std::cout << consts::fpow(3.1415926f, -0.5f) << std::endl;
     return 0;
 }
