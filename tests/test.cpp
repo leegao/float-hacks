dc3ff0dc0b1160b0dcd26790e18eaac412639ca2
leegao <lg342@cornell.edu>
2016-12-14 18:49:32 -0800

    time test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <iostream>
+#include <chrono>
 #include "../floathacks/hacks.h"
 
 using floathacks::Ratio;
@@ -48,6 +49,76 @@ void test_log(float threshold = 1e-1) {
     std::cout << "Success!" << std::endl;
 }
 
+template <class R>
+void test_time_pow(float s) {
+    std::cout << "Time test for pow(" << R::value() << ")" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    float sum = 0;
+    for (float n = s; n < 100000; n += 0.01) {
+        sum += floathacks::fpow<R>::estimate(n);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << sum << std::endl;
+    std::cout << "floathacks::fpow Took " << duration << " ms" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    sum = 0;
+    for (float n = s; n < 100000; n += 0.01) {
+        sum += powf(n, R::value());
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << sum << std::endl;
+    std::cout << "std::pow Took " << duration << " ms" << std::endl;
+}
+
+void test_time_exp(float s) {
+    std::cout << "Time test for exp(n)" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    float sum = 0;
+    for (float n = s; n < 100000; n += 0.01) {
+        sum += floathacks::fexp(n);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << sum << std::endl;
+    std::cout << "floathacks::fexp Took " << duration << " ms" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    sum = 0;
+    for (float n = s; n < 100000; n += 0.01) {
+        sum += expf(n);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << sum << std::endl;
+    std::cout << "std::exp Took " << duration << " ms" << std::endl;
+}
+
+void test_time_log(float s) {
+    std::cout << "Time test for log(n)" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    float sum = 0;
+    for (float n = s; n < 100000; n += 0.01) {
+        sum += floathacks::flog(n);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << sum << std::endl;
+    std::cout << "floathacks::flog Took " << duration << " ms" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    sum = 0;
+    for (float n = s; n < 100000; n += 0.01) {
+        sum += expf(n);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << sum << std::endl;
+    std::cout << "std::log Took " << duration << " ms" << std::endl;
+}
+
 int main() {
     test_pow<Ratio<1, 1>>();
     test_pow<FLOAT(-0.5f)>();
@@ -56,4 +127,10 @@ int main() {
     test_pow<FLOAT(0.2)>();
     test_exp();
     test_log();
+    float start = (float)(rand() % 10) / 10.0f;
+    test_time_pow<FLOAT(2)>(start);
+    test_time_pow<FLOAT(-0.5f)>(start);
+    test_time_pow<FLOAT(5)>(start);
+    test_time_exp(start);
+    test_time_log(start);
 }
