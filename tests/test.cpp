c3faa417c2df0d4a74e8f28ad41c039c5be4c6ee
leegao <lg342@cornell.edu>
2016-12-14 18:13:37 -0800

    more test cases

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,23 +1,31 @@
-#include <assert.h>
 #include <cmath>
 #include <iostream>
 #include "../floathacks/hacks.h"
 
 using floathacks::Ratio;
 
-void assertWithinRange(float expected, float actual, float threshold = 1e-1) {
-    assert(fabs(expected - actual) < threshold);
+inline bool assertWithinRange(float expected, float actual, float threshold = 1e-1) {
+    return fabs(expected - actual)/fabs(actual) < threshold;
 }
 
 template <class R>
-void test_pow() {
+void test_pow(float threshold = 1e-1) {
     std::cout << "Testing pow(n, " << R::value() << ");" << std::endl;
     for (float n = 0.1; n < 10; n += 0.01) {
-        assertWithinRange(floathacks::fpow<R>::estimate(n), n);
+        if (!assertWithinRange(floathacks::fpow<R>::estimate(n), powf(n, R::value()), threshold)) {
+            std::cout << "Test failed for pow(" << n << ", " << R::value() << ");"
+                      << " expected " << powf(n, R::value())
+                      << ", but got " << floathacks::fpow<R>::estimate(n) << " instead" << std::endl;
+            return;
+        }
     }
     std::cout << "Success!" << std::endl;
 }
 
 int main() {
     test_pow<Ratio<1, 1>>();
+    test_pow<FLOAT(-0.5f)>();
+    test_pow<FLOAT(2)>();
+    test_pow<FLOAT(5)>(2e-1); // relative error is a bit higher
+    test_pow<FLOAT(0.2)>();
 }
